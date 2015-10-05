d834e5b0c78690083a3a306cd0a81813d42d002f
Jontae Pierce <jontae.pierce@gmail.com>
2015-10-05 19:39:51 -0400

    Added hours mins and secs, need to style and line everything up

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+//Gather data from the clock
+
+  
+  setInterval( function() {
+     var seconds = new Date().getSeconds();
+     $("#seconds").html(( seconds < 10 ? "0" : "" ) + seconds);
+     },1000);
+ 
+
+
+  setInterval( function() {
+     var minutes = new Date().getMinutes();
+     $("#minutes").html(( minutes < 10 ? "0" : "" ) + minutes);
+     },1000);
+  
+  
+  
+  setInterval( function() {
+     var hours = new Date().getHours();
+     $("#hours").html(( hours < 10 ? "0" : "" ) + hours);
+     }, 1000);
+
+
+
