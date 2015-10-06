769bcc01a01bf3b4e420f9f18bc545d6fbf351ec
Jontae Pierce <jontae.pierce@gmail.com>
2015-10-05 20:30:34 -0400

    final clock product

--- js/main.js
+++ js/main.js
@@ -1,25 +1,26 @@
 
-//Gather data from the clock
+//Gather data from the clock and create timer
 
-  
-  setInterval( function() {
+  var gotSecs = setInterval( function() {
      var seconds = new Date().getSeconds();
      $("#seconds").html(( seconds < 10 ? "0" : "" ) + seconds);
      },1000);
  
 
 
-  setInterval( function() {
+  var gotMins = setInterval( function() {
      var minutes = new Date().getMinutes();
      $("#minutes").html(( minutes < 10 ? "0" : "" ) + minutes);
      },1000);
   
   
   
-  setInterval( function() {
+  var gotHours = setInterval( function() {
      var hours = new Date().getHours();
      $("#hours").html(( hours < 10 ? "0" : "" ) + hours);
      }, 1000);
+  
+
 
 
 
